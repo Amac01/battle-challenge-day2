ff3291417f14cd035d7d6668472b84f61c0d0416
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2018-11-14 16:20:07 +0000

    entering our names and seeing them

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,10 @@
-feature "homepage" do
-  scenario "viewing the homepage" do
+
+feature "Enter player names" do
+  scenario "submitting names" do
     visit '/'
-    expect(page).to have_content "Testing infrastructure working!"
+    fill_in :player_1_name, with: 'Will'
+    fill_in :player_2_name, with: 'Alpha'
+    click_button 'Submit'
+    expect(page).to have_content "Will vs. Alpha"
   end
 end
