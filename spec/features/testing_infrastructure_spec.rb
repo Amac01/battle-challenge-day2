c83106e42ede7935eae9c2c0efa30492849afd9f
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2018-11-15 15:32:28 +0000

    tested to see Player 2's Hit Points

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,3 +8,13 @@ feature "Enter player names" do
     expect(page).to have_content "Will vs. Alpha"
   end
 end
+
+feature "player 2's hit Points" do
+  scenario "display player 2's hit points " do
+    visit '/'
+    fill_in :player_1_name, with: 'Wayne'
+    fill_in :player_2_name, with: 'Alpha'
+    click_button 'Submit'
+    expect(page).to have_content "Alpha - Hit points"
+  end
+end
