62c2ea6527af36c15186d925d8a138a35be2fd36
Alpha Mackie <alphamackie@alphas-mbp.lan>
2018-12-27 22:29:16 +0000

    wrote the test for the attack on player2, created the attack.erb file a route on play.erb

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -12,3 +12,11 @@ feature "player 2's hit Points" do
     expect(page).to have_content "Alpha - Hit points"
   end
 end
+
+feature "Attacking" do
+  scenario "attack player 2 " do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content 'Will attacked Alpha'
+  end
+end
