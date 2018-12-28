6df6797b56686e63aac715fbeb859aa89bf6091c
Alpha Mackie <alphamackie@alphas-mbp.lan>
2018-12-28 13:17:45 +0000

    remove session from app.rb  and introduced instances of Player class

--- app.rb
+++ app.rb
@@ -23,8 +23,8 @@ class Battle < Sinatra::Base
   # The browser reads the Location header and sends a GET request to the
   # specified path.
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
@@ -36,14 +36,14 @@ class Battle < Sinatra::Base
   #
   # play.erb is rendered on the browser with the correct data.
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
